4cf44a5e1d8f9eee75e69846ea25345ba96752b3
Jesus Arambula <jesus.eliel.arambula@gmail.com>
2021-01-30 20:29:17 -0800

    Fourth Commit
    Created enemy (only 1 at this moment), gave it a fixed speed, and created main game loop (moving enemy while game is running and boundary checking)

--- main.py
+++ main.py
@@ -29,17 +29,26 @@ player.setheading(90)
 # Player speed
 playerspeed = 15
 
+enemy = turtle.Turtle()
+enemy.color('red')
+enemy.shape('circle')
+enemy.penup()
+enemy.speed(0)
+enemy.setposition(-200, 250)
+
+enemyspeed = 2
+
 def move_left():
     x = player.xcor()
     x -= playerspeed
-    if x < -260:
+    if x < -280:
         x = -280
     player.setx(x)
 
 def move_right():
     x = player.xcor()
     x += playerspeed
-    if x > 260:
+    if x > 280:
         x = 280
     player.setx(x)
 
@@ -48,6 +57,27 @@ def move_right():
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
+
+# Main Game Loop
+while True:
+    # Move enemy
+    x = enemy.xcor()
+    x += enemyspeed
+    enemy.setx(x)
+
+    # Move enemy back and down
+    if enemy.xcor() > 280:
+        enemyspeed *= -1
+        y = enemy.ycor()
+        y -= 20
+        enemy.sety(y)
+
+    if enemy.xcor() < -280:
+        enemyspeed *= -1
+        y = enemy.ycor()
+        y -= 20
+        enemy.sety(y)
+
 
 
 input("Press Enter to continue...")
