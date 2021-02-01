be4d853f70f227538b92ee396fff8d3692a0463f
Jesus Arambula <jesus.eliel.arambula@gmail.com>
2021-01-31 20:30:58 -0800

    Fifth Commit
    Created bullet:
    - bullet speed
    - bullet state (behavior of the bullet)
    - keyboard binding (spacebar)
    - fire bullet function

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ player.setposition(0, -250)
 player.setheading(90)
 
 # Player speed
-playerspeed = 15
+player_speed = 15
 
 enemy = turtle.Turtle()
 enemy.color('red')
@@ -36,47 +36,84 @@ enemy.penup()
 enemy.speed(0)
 enemy.setposition(-200, 250)
 
-enemyspeed = 2
+enemy_speed = 2
 
+# Create player's bullet
+bullet = turtle.Turtle()
+bullet.color('yellow')
+bullet.shape('triangle')
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(.5, .5)
+bullet.hideturtle()
+
+bullet_speed = 20
+
+# Define state of bullet
+# ready - ready to fire
+# fire - bullet is firing
+bullet_state = "ready"
+
+# Move left and right
 def move_left():
     x = player.xcor()
-    x -= playerspeed
+    x -= player_speed
     if x < -280:
         x = -280
     player.setx(x)
-
 def move_right():
     x = player.xcor()
-    x += playerspeed
+    x += player_speed
     if x > 280:
         x = 280
     player.setx(x)
-
+def fire_bullet():
+    global bullet_state
+    if bullet_state == "ready":
+        bullet_state = "fire"
+        # Move bullet above player
+        y = player.ycor() + 10
+        x = player.xcor()
+        bullet .setposition(x, y)
+        bullet.showturtle()
 
 # Keyboard bindings
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
+turtle.onkey(fire_bullet, "space")
 
 # Main Game Loop
 while True:
     # Move enemy
     x = enemy.xcor()
-    x += enemyspeed
+    x += enemy_speed
     enemy.setx(x)
 
     # Move enemy back and down
     if enemy.xcor() > 280:
-        enemyspeed *= -1
+        enemy_speed *= -1
         y = enemy.ycor()
         y -= 20
         enemy.sety(y)
 
     if enemy.xcor() < -280:
-        enemyspeed *= -1
+        enemy_speed *= -1
         y = enemy.ycor()
         y -= 20
         enemy.sety(y)
+
+    # Move bullet
+    if bullet_state == "fire":
+        y = bullet.ycor()
+        y += bullet_speed
+        bullet.sety(y)
+
+    # Check to see if bullet has gone to the top
+    if bullet.ycor() > 275:
+        bullet.hideturtle()
+        bullet_state = "ready"
 
 
 
